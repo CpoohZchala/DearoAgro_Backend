d0a1c06627fdf8721f406c43e661224f65b8a8f8
Cpooh <cpooh2001@gmail.com>
2025-05-22 15:55:51 +0530

    updated crop  model

--- models/CropUpdate.js
+++ models/CropUpdate.js
@@ -4,9 +4,9 @@ const cropSchema = new mongoose.Schema({
   memberId: { type: String, required: true },
   addDate: { type: String, required: true },
   description: { type: String, required: true },
-  fertilizerType: { type: String },      // New
-  fertilizerAmount: { type: Number },    // New
-  fertilizerUnit: { type: String },      // New
+  fertilizerType: { type: String },      
+  fertilizerAmount: { type: Number },    
+  fertilizerUnit: { type: String },     
 });
 
 const CropData = mongoose.model("CropData", cropSchema);
