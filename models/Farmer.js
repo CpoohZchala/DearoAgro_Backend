b0b512637cbd80feb0c84b279b43eb11e073d5d2
Cpooh <cpooh2001@gmail.com>
2025-05-22 09:58:45 +0530

    updated farmer model

--- models/Farmer.js
+++ models/Farmer.js
@@ -7,7 +7,7 @@ const farmerSchema = new mongoose.Schema({
     groupId: { type: mongoose.Schema.Types.ObjectId, ref: 'Group', default: null },
     groupName: { type: String, default: null },
     profileImage: { type: String, default: "" },
-    userType: { type: String,default: "" },
+    userType: { type: String,default: "Farmer" },
     createdAt: { type: Date, default: Date.now },
 });
 
