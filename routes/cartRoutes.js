fd995c6acbabb908e6ee8ddbc22acd9d9e73ae98
Cpooh <cpooh2001@gmail.com>
2025-06-03 22:04:39 +0530

    updated crop updates

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { CartController } = require('../controllers/cartController');
+const CartController = require('../controllers/cartController'); // Correct import
+
+const cartController = new CartController(); // Instantiate the class
 
 const router = express.Router();
-const cartController = new CartController();
 
-router.post('/add', cartController.addToCart);      // Add product to cart
-router.delete('/remove/:id', cartController.removeFromCart); // Remove product from cart
+router.post('/add', cartController.addToCart); // Add to cart route
+router.delete('/remove/:id', cartController.removeFromCart); // Remove from cart route
+router.get('/', cartController.getCart); // Get cart details route
 
 module.exports = router;
