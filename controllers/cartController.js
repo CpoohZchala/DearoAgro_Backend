e43d80d3399160b38249734b5c0aa83fafaa7e2f
Cpooh <cpooh2001@gmail.com>
2025-06-03 22:15:30 +0530

    updated cart updates

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,26 +1,20 @@
 class CartController {
-    constructor(cartModel) {
-        this.cartModel = cartModel;
+    constructor() {
+        this.addToCart = this.addToCart.bind(this);
+        this.removeFromCart = this.removeFromCart.bind(this);
+        this.getCart = this.getCart.bind(this);
     }
 
     addToCart(req, res) {
-        const { productId, quantity } = req.body;
-        const result = this.cartModel.addItem(productId, quantity);
-        if (result.success) {
-            res.status(200).json({ message: 'Product added to cart', cart: result.cart });
-        } else {
-            res.status(400).json({ message: result.message });
-        }
+        res.status(200).json({ message: 'Product added to cart' });
     }
 
     removeFromCart(req, res) {
-        const { productId } = req.params;
-        const result = this.cartModel.removeItem(productId);
-        if (result.success) {
-            res.status(200).json({ message: 'Product removed from cart', cart: result.cart });
-        } else {
-            res.status(400).json({ message: result.message });
-        }
+        res.status(200).json({ message: 'Product removed from cart' });
+    }
+
+    getCart(req, res) {
+        res.status(200).json({ message: 'Cart details fetched' });
     }
 }
 
