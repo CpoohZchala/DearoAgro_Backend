2c8218d80ed200f679a63dc7a2bfd9610353562c
Cpooh <cpooh2001@gmail.com>
2025-08-05 12:04:11 +0530

    updated stock details

--- routes/stockRoutes.js
+++ routes/stockRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Stock = require('../models/Stock.model');
+const Stock = require('../models/Stock');
 
 // GET all stocks
 router.get('/', async (req, res) => {
