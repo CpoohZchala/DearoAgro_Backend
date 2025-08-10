8ceedbf6c27ce3530a2220e34064da3f5b94abf9
Cpooh <cpooh2001@gmail.com>
2025-08-10 09:35:25 +0530

    updated inquiry details

--- models/Stock.js
+++ models/Stock.js
@@ -11,6 +11,10 @@ const stockSchema = new mongoose.Schema({
   harvestDate: { type: Date, required: true },
   currentPrice: { type: Number, default: 0 },
   quantity: { type: Number, default: 0 },
+  isProductListed: {
+    type: Boolean,
+    default: false
+  },
   createdAt: { type: Date, default: Date.now }
 }, {
   timestamps: true 
