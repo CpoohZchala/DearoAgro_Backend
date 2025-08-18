af7f22f5627533cd8498d4671d4221241b52ba04
Cpooh <cpooh2001@gmail.com>
2025-08-18 12:16:40 +0530

    updated  crop model

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,22 +1,12 @@
 const express = require('express');
-const { getCart, addToCart, updateCartItem, removeFromCart, clearCart } = require('../controllers/cartController');
+const router = express.Router();
+const cartController = require('../controllers/cartController');
 const authenticate = require('../middleware/authenticate');
 
-const router = express.Router();
-
-// Protect all routes with buyer authentication
-router.use((req, res, next) => {
-  if (req.user.userType !== 'Buyer') {
-    return res.status(403).json({ message: 'Access denied. Buyers only.' });
-  }
-  next();
-});
-
-router.get('/cart', authenticate,getCart);
-router.post('/cart/add', authenticate,addToCart);
-router.put('/cart/update/:itemId', authenticate,updateCartItem);
-router.delete('/cart/remove/:itemId', authenticate,removeFromCart);
-router.delete('/cart/clear', authenticate,clearCart);
- // Clear cart
+router.get('/cart', authenticate, cartController.getCart);
+router.post('/cart/add', authenticate, cartController.addToCart);
+router.put('/cart/update/:itemId', authenticate, cartController.updateCartItem);
+router.delete('/cart/remove/:itemId', authenticate, cartController.removeFromCart);
+router.delete('/cart/clear', authenticate, cartController.clearCart);
 
 module.exports = router;
