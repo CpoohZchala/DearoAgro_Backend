d7c97215d91341aa04e55eb314beb40efd261d62
Cpooh <cpooh2001@gmail.com>
2025-08-18 11:56:04 +0530

    updated  crop model

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require('../controllers/cartController');
+const cartController = require('../controllers/cartController');
 const authenticate = require('../middleware/authenticate');
 
 const router = express.Router();
@@ -13,11 +13,11 @@ router.use((req, res, next) => {
   next();
 });
 
-router.get('/cart', authenticate, cartController.getCart);
-router.post('/cart/add', authenticate, cartController.addToCart);
-router.put('/cart/update/:itemId', authenticate, cartController.updateCartItem);
-router.delete('/cart/remove/:itemId', authenticate, cartController.removeFromCart);
-router.delete('/cart/clear', authenticate, cartController.clearCart);
+router.get('/cart', authenticate,getCart);
+router.post('/cart/add', authenticate,addToCart);
+router.put('/cart/update/:itemId', authenticate,updateCartItem);
+router.delete('/cart/remove/:itemId', authenticate,removeFromCart);
+router.delete('/cart/clear', authenticate,clearCart);
  // Clear cart
 
 module.exports = router;
