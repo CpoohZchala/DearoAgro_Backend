e55b3cbab867d11d1b00a3acc791259630e60074
Cpooh <cpooh2001@gmail.com>
2025-08-18 19:54:52 +0530

    Updated folder structure

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ connectDB();
 app.use(cors({
   origin: [
     'https://dearoagro-admin-dashboard.onrender.com',
-    'http://localhost:5174'
+    'http://localhost:5173'
   ],
   credentials: true
 }));
